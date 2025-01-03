3a4418b8ed080d1e75c485780c7f9b63d488a605
Roland Boon <roland@rolandboon.com>
2025-01-03 14:39:53 +0100

    Fixes installing npm

--- app/installations/essential/node-js.sh
+++ app/installations/essential/node-js.sh
@@ -1,2 +1,3 @@
-install_if_not_present "nodejs-lts-iron" "sudo pacman -S nodejs-lts-iron npm --noconfirm --noprogressbar --quiet"
+install_if_not_present "nodejs-lts-iron" "sudo pacman -S nodejs-lts-iron --noconfirm --noprogressbar --quiet"
+install_if_not_present "npm" "sudo pacman -S npm --noconfirm --noprogressbar --quiet"
 sudo npm --global --silent install yarn
