9fd045b7bf3c43a411130302f6e3422ba7997a31
Roland Boon <roland@rolandboon.com>
2025-01-07 09:44:56 +0100

    Extends kubectl cluster management utilities

--- app/installations/optional/kubectl.sh
+++ app/installations/optional/kubectl.sh
@@ -1,1 +1,3 @@
 install_if_not_present "kubectl" "yay -S kubectl --noconfirm --noprogressbar --quiet"
+install_if_not_present "helm" "yay -S helm --noconfirm --noprogressbar --quiet"
+install_if_not_present "eksctl" "yay -S eksctl --noconfirm --noprogressbar --quiet"
