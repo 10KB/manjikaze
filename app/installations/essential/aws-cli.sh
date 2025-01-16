c83827a0b005f1cbbc3c01746887b4433e85ecb7
Roland Boon <roland@rolandboon.com>
2025-01-16 20:07:11 +0100

    Adds aws sam cli

--- app/installations/essential/aws-cli.sh
+++ app/installations/essential/aws-cli.sh
@@ -1,1 +1,2 @@
 install_if_not_present "aws-cli-v2" "yay -S aws-cli-v2 --noconfirm --noprogressbar --quiet"
+install_if_not_present "aws-sam-cli" "yay -S aws-sam-cli --noconfirm --noprogressbar --quiet"
