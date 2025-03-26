1beb303100955298be9077c7f6ff1da63a43c9ca
Roland Boon <roland@rolandboon.com>
2025-03-26 09:50:19 +0100

    Adds img2pdf extension for pdf arranger

--- app/installations/recommended/pdfarranger.sh
+++ app/installations/recommended/pdfarranger.sh
@@ -1,1 +1,3 @@
 install_if_not_present "pdfarranger" "yay -S pdfarranger --noconfirm --noprogressbar --quiet"
+install_if_not_present "img2pdf" "sudo pacman -S img2pdf --noconfirm --noprogressbar --quiet"
+install_if_not_present "shrinkpdf" "yay -S shrinkpdf --noconfirm --noprogressbar --quiet"
