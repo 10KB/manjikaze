bf4d0c39865b6c3c14853f47f26dcbcf7edb74b0
Roland Boon <roland@rolandboon.com>
2025-04-08 15:17:38 +0200

    Pin alacritty to dock instead of default terminal

--- app/installations/recommended/alacritty.sh
+++ app/installations/recommended/alacritty.sh
@@ -4,3 +4,31 @@ if [ ! -d ~/.config/alacritty ]; then
     mkdir -p ~/.config/alacritty
     cp "$MANJIKAZE_DIR/configs/alacritty.toml" ~/.config/alacritty/alacritty.toml
 fi
+
+# Pin Alacritty to dock and remove default terminal if using GNOME
+if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
+    status "Pinning Alacritty to dock and removing default terminal..."
+
+    # Get current favorites
+    current_favorites=$(gsettings get org.gnome.shell favorite-apps)
+
+    # Remove the default terminal and trailing bracket
+    new_favorites=$(echo "$current_favorites" | sed "s/'org.gnome.Terminal.desktop', //g" | sed "s/'gnome-terminal.desktop', //g" | sed 's/]$//')
+
+    # Add Alacritty if not already in favorites
+    if [[ "$new_favorites" != *"'org.gnome.Alacritty.desktop'"* ]] && [[ "$new_favorites" != *"'Alacritty.desktop'"* ]]; then
+        # Check if we need to add a comma
+        if [[ "$new_favorites" != *"," ]]; then
+            new_favorites="${new_favorites}, "
+        fi
+
+        # Add Alacritty and closing bracket
+        new_favorites="${new_favorites}'Alacritty.desktop']"
+    else
+        # Just add the closing bracket back
+        new_favorites="${new_favorites}]"
+    fi
+
+    # Update favorites
+    gsettings set org.gnome.shell favorite-apps "$new_favorites"
+fi
