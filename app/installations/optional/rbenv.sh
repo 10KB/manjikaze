078f63857925e19be2b5aade69b30fed2c7ab29c
Martijn van der Heiden <martijn@10kb.nl>
2025-04-15 13:00:31 +0200

    Add rbenv as a zsh plugin instead

--- app/installations/optional/rbenv.sh
+++ app/installations/optional/rbenv.sh
@@ -2,11 +2,17 @@ install_package 'rbenv' aur
 install_package 'ruby-build' aur
 install_package 'ruby-erb' repo
 
-# Add rbenv to PATH for both bash and zsh
-for shell in bash zsh; do
-    if [ -f "$HOME/.${shell}rc" ]; then
-        if ! grep -q "rbenv init" "$HOME/.${shell}rc"; then
-            echo 'eval "$(rbenv init -)"' >> "$HOME/.${shell}rc"
-        fi
+# Create oh-my-zsh custom plugins directory for rbenv
+mkdir -p "$HOME/.oh-my-zsh/custom/plugins/rbenv"
+
+# Create rbenv plugin file
+cat > "$HOME/.oh-my-zsh/custom/plugins/rbenv/rbenv.plugin.zsh" << 'EOF'
+eval "$(rbenv init -)"
+EOF
+
+# Add rbenv to plugins list in .zshrc if it's not already there
+if [ -f "$HOME/.zshrc" ]; then
+    if ! grep -q "plugins=.*rbenv" "$HOME/.zshrc"; then
+        sed -i 's/^plugins=(\(.*\))/plugins=(\1 rbenv)/' "$HOME/.zshrc"
     fi
-done
+fi
