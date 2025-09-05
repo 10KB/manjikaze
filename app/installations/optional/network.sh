fca9e3b04720d7502195867b8dbccafe70551f61
Roland Boon <roland@rolandboon.com>
2025-09-05 14:45:32 +0200

    Adds additional networking tools and move to optional

---
+++ app/installations/optional/network.sh
@@ -0,0 +1,11 @@
+# Netcat to debug TCP connections
+install_package "gnu-netcat" repo
+
+# Parallel to run parallel network requests
+install_package "parallel" repo
+
+# Bind to resolve DNS
+install_package "bind" repo
+
+# Nmap to scan ports
+install_package "nmap" repo
